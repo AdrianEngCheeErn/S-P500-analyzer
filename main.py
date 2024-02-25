329abd5f2502d2dedddbd53836ddd078bb991dfa
AdrianEngCheeErn <adrianengcheeern@gmail.com>
2024-02-25 23:51:36 +1100

    update

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-st.title('S&P 500 App')
-
-st.markdown("""
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
-""")
-
-st.sidebar.header('User Input Features')
+#pandas html scaper
+df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', header = 0)[0]
 
 
-df = html = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', header = 0)[0]
-sector = df.groupby('GICS Sector')
+st.title('S&P 500 Analyzer')
+
+st.sidebar.header('Filter by GICS sector')
 sorted_sector_unique = sorted( df['GICS Sector'].unique() )
 selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
 df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
-st.header('Display Companies in Selected Sector')
+
+#Display companies based on selected GICS Sector
+st.header('Filtered Companies')
 st.dataframe(df_selected_sector)
+
+#Predict stock prices
+st.header('Stock Prediction')
+stock_symbols = df["Symbol"]
+input_symbol =  st.selectbox("Select symbol of stock", stock_symbols)
