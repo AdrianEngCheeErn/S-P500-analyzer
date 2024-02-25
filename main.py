5bb6b74868c9bbfed0bdd732737eb66631153057
AdrianEngCheeErn <adrianengcheeern@gmail.com>
2024-02-25 17:22:27 +1100

    update

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ st.markdown("""
 """)
 
 st.sidebar.header('User Input Features')
+
+
+df = html = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', header = 0)[0]
+sector = df.groupby('GICS Sector')
+sorted_sector_unique = sorted( df['GICS Sector'].unique() )
+selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
+df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
+st.header('Display Companies in Selected Sector')
+st.dataframe(df_selected_sector)
