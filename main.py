9dabf2110dcbc19eb1ad80575d41249f150f0922
AdrianEngCheeErn <adrianengcheeern@gmail.com>
2024-02-28 11:47:14 +1100

    update

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import precision_score
-import xgboost as xgb 
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 import time
 
 
@@ -45,14 +45,39 @@ if input_symbol != "-":
         ax.plot(historical_prices.index, historical_prices['Close'], label='Stock Value')
         ax.set_xlabel('Time')
         ax.set_ylabel('Stock Value')
-        ax.set_title('Apple Stock Value')
+        ax.set_title('Stock Value')
         ax.legend(loc='upper left')
         ax.tick_params(axis='x', rotation=45)
         plot.pyplot(fig)
         time.sleep(60)
 
+    n_years = st.slider('Years of prediction:', 1, 4)
+    period = n_years * 365
 
-#Stock price prediction TODO
+    @st.cache
+    def load_data(ticker):
+        data = yf.download(ticker, START, TODAY)
+        data.reset_index(inplace=True)
+        return data
+    
+    df_train = data[['Date','Close']]
+    df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+    m = Prophet()
+    m.fit(df_train)
+    future = m.make_future_dataframe(periods=period)
+    forecast = m.predict(future)
+
+    st.subheader('Forecast data')
+    st.write(forecast.tail())
+        
+    st.write(f'Forecast plot for {n_years} years')
+    fig1 = plot_plotly(m, forecast)
+    st.plotly_chart(fig1)
+
+    st.write("Forecast components")
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
 
 
 
